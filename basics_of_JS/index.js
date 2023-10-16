a7ed4d7b52699f87b2c0ae2224ca6a34adf79251
Aakash Yadav <ay9675928687@gmail.com>
2023-10-16 14:43:55 +0530

    practicing

--- basics_of_JS/index.js
+++ basics_of_JS/index.js
@@ -1,7 +1,18 @@
 let myDate = new Date();
 console.log(myDate);
+console.log(typeof myDate);
 console.log(myDate.toString());
-// console.log(myDate.toISOString());
+console.log(myDate.toISOString());
 console.log(myDate.toJSON());
-// console.log(myDate.toLocaleDateString())
-// console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+let myCreatedDate1 = new Date("2023-01-22");
+let myCreatedDate2 = new Date("23-01-2023");
+console.log(myCreatedDate1);
+console.log(myCreatedDate2);
+
+let myCreatedDate3 = new Date("2023-01-22");
+console.log(myCreatedDate3.toDateString());
+
+let myCreatedDate4 = new Date(2023, 0, 22);
+console.log(myCreatedDate4.toDateString());
