3e5956ec5847ba348f8576c9d94b0f42e3868650
Aakash Yadav <ay9675928687@gmail.com>
2023-10-16 14:21:54 +0530

    first commit

---
+++ basics_of_JS/index.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+// console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
