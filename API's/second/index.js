3f33c48b035d9dd40f88f6751a0ca2cb001f4df5
Aakash Yadav <ay9675928687@gmail.com>
2023-10-23 15:08:27 +0530

    practicing async

--- API's/second/index.js
+++ API's/second/index.js
@@ -7,7 +7,7 @@ const promiseOne = new Promise((res, reject) => {
 promiseOne.then(() => {
   console.log("promise 1 consumed");
 });
-
+// ##########################################
 const promiseTwo = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log("resolve task 2");
@@ -16,7 +16,7 @@ const promiseTwo = new Promise((resolve, reject) => {
 }).then(() => {
   console.log("task 2 has done");
 });
-
+// ###################################
 const promiseThird = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve({ user: "aakash yadav", email: "yadavaakash8687@gamil.com" });
@@ -26,7 +26,7 @@ const promiseThird = new Promise((resolve, reject) => {
 promiseThird.then((user) => {
   console.log(user);
 });
-
+// ####################################
 const promiseFour = new Promise((resolve, reject) => {
   setTimeout(() => {
     let error = false;
@@ -45,7 +45,7 @@ promiseFour
   .catch((error) => {
     console.log(error);
   });
-
+// #########################################
 const promiseFive = new Promise((resolve, reject) => {
   setTimeout(() => {
     let error = false;
@@ -66,4 +66,60 @@ promiseFive
   })
   .catch((error) => {
     console.log(error);
+  });
+// ############################################
+
+const promiseSix = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      console.log({ languaeg: "javascript", framework: "react" });
+    } else {
+      reject("promise 6 was rejected");
+    }
+  }, 1000);
+});
+
+async function dealPromiseSix() {
+  try {
+    const response = await promiseSix;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+dealPromiseSix();
+
+// #######################################
+
+async function getUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("error");
+  }
+}
+
+getUsers();
+
+// ########################
+
+const jsonString = '{"name": "John", "age": 30}';
+const jsObject = JSON.parse(jsonString); // Parses the JSON string into a JavaScript object
+console.log(jsObject.name);
+
+// ######################
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("errror");
   });
