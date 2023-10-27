f072a5b41d11cc617ad6f7fa7be533edb28cc6d5
Aakash Yadav <ay9675928687@gmail.com>
2023-10-27 15:32:22 +0530

    initial setup of draggable

--- crud_operations/script.js
+++ crud_operations/script.js
@@ -3,10 +3,8 @@ submitButton.addEventListener("click", Submit);
 
 function Submit() {
   retrieveData();
-  var readData = readingDateFromLocalStorage(dataEntered);
-  insert(readData);
-  console.log(dataEntered);
-  console.log(readData);
+  // var readData = readingDateFromLocalStorage(dataEntered);
+  // insert(readData);
 }
 
 // #retriving data from form
@@ -33,42 +31,42 @@ function retrieveData() {
   }
 }
 
-#data in local storage
+// #data in local storage
 
-function readingDateFromLocalStorage(dataEntered) {
-  // #setting data in localstorage
-  localStorage.setItem("Name", dataEntered[0]);
-  localStorage.setItem("Job", dataEntered[1]);
-  localStorage.setItem("Experience", dataEntered[2]);
+// function readingDateFromLocalStorage(dataEntered) {
+//   // #setting data in localstorage
+//   localStorage.setItem("Name", dataEntered[0]);
+//   localStorage.setItem("Job", dataEntered[1]);
+//   localStorage.setItem("Experience", dataEntered[2]);
 
-  //   #getting data from localstorage
+//   //   #getting data from localstorage
 
-  var gname = localStorage.getItem("Name", name);
-  var gjob = localStorage.getItem("Job", job);
-  var gexperience = localStorage.getItem("Experience", experience);
+//   var gname = localStorage.getItem("Name", name);
+//   var gjob = localStorage.getItem("Job", job);
+//   var gexperience = localStorage.getItem("Experience", experience);
 
-  var arr = [gname, gjob, gexperience];
-  return arr;
-}
+//   var arr = [gname, gjob, gexperience];
+//   return arr;
+// }
 
-function readingDateFromLocalStorage(dataEntered) {
-  //   // #setting data in localstorage
+// function readingDateFromLocalStorage(dataEntered) {
+//   //   // #setting data in localstorage
 
-  var [name, job, experience] = dataEntered;
-  var setname = localStorage.setItem("Name", name);
-  var setjob = localStorage.setItem("Job", job);
-  var setexperience = localStorage.setItem("Experience", experience);
+//   var [name, job, experience] = dataEntered;
+//   var setname = localStorage.setItem("Name", name);
+//   var setjob = localStorage.setItem("Job", job);
+//   var setexperience = localStorage.setItem("Experience", experience);
 
-  //   //   #getting data from localstorage
+//   //   //   #getting data from localstorage
 
-  var gname = localStorage.getItem("Name", setname);
-  var gjob = localStorage.getItem("Job", setjob);
-  var gexperience = localStorage.getItem("Experience", setexperience);
+//   var gname = localStorage.getItem("Name", setname);
+//   var gjob = localStorage.getItem("Job", setjob);
+//   var gexperience = localStorage.getItem("Experience", setexperience);
 
-  var arr = [gname, gjob, gexperience];
-  return arr;
-}
+//   var arr = [gname, gjob, gexperience];
+//   return arr;
+// }
 
-function insert(readData) {
-  var row = table.insertRow();
-}
+// function insert(readData) {
+//   var row = table.insertRow();
+// }
