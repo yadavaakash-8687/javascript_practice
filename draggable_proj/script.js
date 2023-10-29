9e769e519a5bb1a864d44a25ad1ab55b7703932e
Aakash Yadav <ay9675928687@gmail.com>
2023-10-29 13:20:39 +0530

    practicing

--- draggable_proj/script.js
+++ draggable_proj/script.js
@@ -4,10 +4,17 @@ const containers = document.querySelectorAll(".container");
 draggables.forEach((draggable) => {
   draggable.addEventListener("dragstart", () => {
     draggable.classList.add("dragging");
-    console.log(draggable.classList);
+    // console.log(draggable.classList);
   });
 
   draggable.addEventListener("dragend", () => {
     draggable.classList.remove("dragging");
+  });
+});
+
+containers.forEach((container) => {
+  container.addEventListener("dragover", () => {
+    const draggable = document.querySelector(".dragging");
+    container.appendChild(draggable);
   });
 });
