bc1b61bfbad44f13a3138df32495b78bb3dc8f03
Aakash Yadav <ay9675928687@gmail.com>
2023-11-01 00:23:17 +0530

    practicing http requests

--- http_requests/index.js
+++ http_requests/index.js
@@ -1,1 +1,21 @@
 import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>hello there this is server 3000 on which we are practicing our basics of http request and maethods to handel the requests done to the server</h1>"
+  );
+});
+app.get("/about", (req, res) => {
+  res.send(
+    "<p>this is the basic information about the wepage which generally tells about the content this webpage is containing.</p>"
+  );
+});
+app.get("/contact", (req, res) => {
+  res.send("<h1>You can contact me here at the localhost </h1>");
+});
+app.listen(port, () => {
+  console.log(`server started at port ${port} `);
+});
